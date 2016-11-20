b4fc77fec01e9d3e4caf2a7c91ba6ec240859a02
JayPrakashThakur <jayprakashthakursnr@gmail.com>
2016-11-20 15:26:15 +0530

    Initial commit

---
+++ app/src/main/java/com/appniche/butterknifeexample/MainActivity.java
@@ -0,0 +1,29 @@
+package com.appniche.butterknifeexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.textView)
+    TextView message;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.button)
+    public void submit(){
+        message.setText("Butter Knife Example Message");
+    }
+}
